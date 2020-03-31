3e3a4f1cf9c85253717e7fe2dae283c366cd45b0
Adam Ingram <everett.ingram22@gmail.com>
2020-03-31 15:10:13 -0400

    Added Constructor for Sony Component

--- src/components/Sony/SonyComponent.jsx
+++ src/components/Sony/SonyComponent.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 
 class SonyComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state={
+            parts: [],
+            message: null,
+        }
+    }
+
     render() {
         return(
             <div><br />
                 <h1 className="shadow p-4 mb-4 bg-white" style={{textAlign: "center"}}>Sony Component</h1>
             </div>
+            
         )
     }
 }
